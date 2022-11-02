011bc4dacc2d49193524b73ecb6271b8f19da549
citalali <rio.redips@gmail.com>
2022-11-02 16:51:25 +0100

    index.html

--- calculate.js
+++ calculate.js
@@ -1,10 +1,23 @@
-function changeBackground(color) {
+/* function changeBackground(color) {
 	document.body.style.background = color;
 }
-window.addEventListener("load", function() {changeBackground('green')});
+
+
+
+const userInput = document.querySelectorAll('button'); // Makes it into a nodelist 
+
 
 function changeScreen() {
-	h1 = blabla;
+	document.getElementById("h1") = userInput;
+
+}
+document.getElementById("one").onclick = alert('green');
+
+*/
+
+document.getElementById("demo").onclick = function() {myFunction();};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
 }
 
-document.addEventListener(#one,)
