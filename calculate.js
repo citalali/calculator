f34694569a566aa29f8255e4fa5a2e8023645580
citalali <rio.redips@gmail.com>
2022-11-02 13:46:54 +0100

    index.html

---
+++ calculate.js
@@ -0,0 +1,10 @@
+function changeBackground(color) {
+	document.body.style.background = color;
+}
+window.addEventListener("load", function() {changeBackground('green')});
+
+function changeScreen() {
+	h1 = blabla;
+}
+
+document.addEventListener(#one,)
