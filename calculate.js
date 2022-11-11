12ea2bef2271c55fddc362e3ecb7ea10336cea38
citalali <rio.redips@gmail.com>
2022-11-11 23:08:34 +0100

    double digits now possible

--- calculate.js
+++ calculate.js
@@ -9,13 +9,16 @@ var operatorsTotal = "";
 var calculateSingelResult = "";
 var numOne;
 var operator = 0;
+var numTwo = 1;
+var lastInput = "";
+var length = 0;
 
 //alert("Warning: Please only use single digits and use * & / before + & -");
 
 function changeH1() {
   numberScreen = numberScreen + numberAtMoment;
   document.getElementById("h1").innerHTML = numberScreen;
-  numbersToArray();
+  // numbersToArray();
 }
 
 function setOnClick() {
@@ -24,30 +27,37 @@ function setOnClick() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
       numberAtMoment = buttons[i].value;
-      changeH1();
+      checkDoubleDigits();
+      //changeH1();
+
+      // numbersToArray();
     };
   }
 }
 
-function numbersToArray() {
+/* function numbersToArray() {
   if (numberAtMoment > 0) {
     numberToCalc.push(numberAtMoment);
+    lastInput = "number";
     // if 2 numbers behind each other push in -1
   } else if (numberAtMoment == "=") {
     /// put function to Calculate in here
     calculateEnd();
+  } else if (numberAtMoment == "reset") {
+    resetCalculator();
   } else {
     operatorsToCalc.push(numberAtMoment);
+    lastInput = "operator";
   }
-}
+} */
 
 function calculateEnd() {
   calculations();
-  numberAtMoment = result;
+  numberAtMoment = "=" + result;
   changeH1();
 }
 
-var numTwo = 1;
+// var numTwo = 1;
 
 function calculations() {
   let i = 0;
@@ -87,6 +97,55 @@ function calculateSingel(op, a, b) {
       calculateSingelResult = "something didnt work";
   }
 }
+
+function resetCalculator() {
+  numberScreen = "";
+  numberAtMoment = "";
+  numberToCalc = [];
+  operatorsToCalc = [];
+  result = "";
+  numbersTotal = "";
+  operatorsTotal = "";
+  calculateSingelResult = "";
+  numOne = "";
+  operator = 0;
+  numTwo = 1;
+  lastInput = "";
+  length = 0;
+  changeH1();
+}
+
+function checkDoubleDigits() {
+  var inputNow = "";
+  if (numberAtMoment > 0) {
+    inputNow = "number";
+    if (inputNow == lastInput) {
+      length = numberToCalc.length - 1;
+      var digits = numberToCalc[length];
+      numberToCalc[length] = digits + numberAtMoment;
+      changeH1();
+    } else {
+      numberToCalc.push(numberAtMoment);
+      lastInput = "number";
+      changeH1();
+    }
+  } else if (numberAtMoment == "=") {
+    calculateEnd();
+  } else if (numberAtMoment == "reset") {
+    resetCalculator();
+  } else {
+    inputNow = "operator";
+    if (inputNow == lastInput) {
+      document.body.style.background = "grey";
+      changeH1();
+    } else {
+      operatorsToCalc.push(numberAtMoment);
+      lastInput = "operator";
+      changeH1();
+    }
+  }
+}
+
 //for loop {}
 //switch statement
 
